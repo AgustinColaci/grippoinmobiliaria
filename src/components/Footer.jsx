9684b01d950e6870cf9a2154c86287bb5cd336bd
Ezequiel <ezepuglisi@gmail.com>
2024-09-25 23:48:49 -0300

    componetizado

---
+++ src/components/Footer.jsx
@@ -0,0 +1,41 @@
+
+const Footer = () => {
+    return (
+        <section className="footer">
+            <div className="footer__logo">
+            </div>
+            <div className="footer__contacto">
+                <h2>Contacto</h2>
+                <ul>
+                    <li className="contacto__wsp">
+                        <a className="link" href="#">+54 11 11111111</a>
+                    </li>
+                    <li className="contacto__mail">
+                        <a className="link" href="#">mail@mail.com</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="footer__ubicacion">
+                <h2>Ubicación</h2>
+                <ul>
+                    <li className="ubicacion">
+                        <a className="link" href="#">+54 11 11111111</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="footer__redes">
+                <h2>Redes sociales</h2>
+                <ul>
+                    <li className="instagram">
+                        <a className="link" href="#">Instagram</a>
+                    </li>
+                    <li className="facebook">
+                        <a className="link" href="#">Facebook</a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    )
+}
+
+export default Footer
