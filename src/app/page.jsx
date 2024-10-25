8481b0654dededc60de420137a23f2e4f8636010
Agustin Colaci <agustincolaci@gmail.com>
2024-10-25 20:22:47 -0300

    libreria slick carousel para slider

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,25 @@
-import Banner from "@/components/Banner";
-import '../globals.css'
+import '../globals.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Banner from '@/components/Banner'
 
 export default async function Home() {
+  try {
+    const properties = await fetch('http://localhost:3000/api/propiedades', {
+      method: 'GET',
+    }).then(res => {
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      return res.json();
+    });
 
-  const properties = await fetch('http://localhost:3000/api/propiedades', {
-    method:'GET',
-  }).then(res => res.json())
-
-  console.log(properties.data)
+    console.log(properties.data);
+  } catch (error) {
+    console.error('Error fetching properties:', error);
+  }
 
   return (
-
     <Banner />
-
   );
 }
