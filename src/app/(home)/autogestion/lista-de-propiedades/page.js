72866bf597d36a3c53255a8c03c076343a5ead6f
Ezequiel <ezepuglisi@gmail.com>
2024-12-30 19:32:48 -0300

    change fetch

--- src/app/(home)/autogestion/lista-de-propiedades/page.js
+++ src/app/(home)/autogestion/lista-de-propiedades/page.js
@@ -1,20 +1,12 @@
 'use server'
 
+import { getAllProperties } from "@/actions/propiedades";
 import ListProperties from "@/components/ListProperties";
 
 const ListaDePropiedades = async () => {
 
   try {
-    const urlentorno = process.env.NEXT_URL_ENTORNO
-    const properties = await fetch(`${urlentorno}/api/propiedades`, {
-      method: 'GET',
-    }).then(res => {
-      if (!res.ok) {
-        throw new Error(`HTTP error! status: ${res.status}`);
-      }
-      return res.json();
-    });
-
+    const properties = await getAllProperties()
 
     return (
       <ListProperties properties={properties.data} />
