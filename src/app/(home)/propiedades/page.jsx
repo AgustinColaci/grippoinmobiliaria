1bbdd00268cc2e0138e59a5d39f10b1be6018578
Ezequiel <ezepuglisi@gmail.com>
2024-12-30 18:50:03 -0300

    checking deploy

--- src/app/(home)/propiedades/page.jsx
+++ src/app/(home)/propiedades/page.jsx
@@ -2,14 +2,16 @@
 
 import FilteredProperties from "@/components/FilteredProperties"
 import Filters from "@/components/Filters"
+import { redirect } from "next/navigation"
 
 // localhost:3000/propiedades
 
 const Propiedades = async () => {
+
+      redirect('/')
+  
   return (
     <div className="container__filtered-properties">
-        <Filters />
-        <FilteredProperties />
     </div>
   )
 }
--- src/app/api/propiedades/similar/route.js
+++ src/app/api/propiedades/similar/route.js
@@ -1,20 +1,21 @@
 import { firestore } from "@/lib/firebase";
 import { collection, query, where, getDocs } from "firebase/firestore";
 
-export async function GET(request, {params}) {
-
-
+export async function GET(request, { params }) {
     try {
-        const url = new URL(request.url)
-        const urlSearchParams = new URLSearchParams(url.searchParams)
-        const zona = urlSearchParams.get('zona')
-        const operacion = urlSearchParams.get('operacion')
-        const id = urlSearchParams.get('id')
+        // Usa request.nextUrl para obtener los parámetros de consulta
+        const { searchParams } = request.nextUrl;
+        const zona = searchParams.get('zona');
+        const operacion = searchParams.get('operacion');
+        const id = searchParams.get('id');
 
-        console.log(id)
+        console.log(id);
 
         if (!zona || !operacion || !id) {
-            return new Response(JSON.stringify({ error: "Missing required parameters" }));
+            return new Response(
+                JSON.stringify({ error: "Missing required parameters" }),
+                { status: 400 }
+            );
         }
 
         // Define la colección y realiza la consulta en Firebase
@@ -22,23 +23,25 @@ export async function GET(request, {params}) {
         const q = query(
             propiedadesRef,
             where("zona", "==", zona),
-            where("tipoOperacion", "==", operacion),
+            where("tipoOperacion", "==", operacion)
         );
 
         // Ejecuta la consulta y prepara los resultados
         const querySnapshot = await getDocs(q);
         const resultados = [];
         querySnapshot.forEach((doc) => {
-            if(Number(doc.id) !== Number(id)){
+            if (Number(doc.id) !== Number(id)) {
                 resultados.push({ id: doc.id, ...doc.data() });
             }
         });
 
         // Devuelve los resultados al cliente
-        return new Response(JSON.stringify(resultados));
+        return new Response(JSON.stringify(resultados), { status: 200 });
     } catch (error) {
         console.error("Error fetching data from Firebase:", error);
-        return new Response(JSON.stringify({ error: "Internal Server Error" }));
+        return new Response(
+            JSON.stringify({ error: "Internal Server Error" }),
+            { status: 500 }
+        );
     }
-
 }
